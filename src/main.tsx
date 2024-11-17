7a8b27045cfc4c614589c015b9f45a3f26648422
moonseonghui <msh9980@naver.com>
2024-11-17 20:18:35 +0900

    delete: 사용하지 않는 파일 정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import queryClient from "./queryClient.ts";
 import { QueryClientProvider } from "@tanstack/react-query";
