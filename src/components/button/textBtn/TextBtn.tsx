e49a64fe7a50ec1e15b1fa2e3abb0a73155b80ae
moonseonghui <msh9980@naver.com>
2024-11-19 01:08:45 +0900

    refactor: TextBtnProps의 색상을 담당하는 props를 type에서 color로 변경

--- src/components/button/textBtn/TextBtn.tsx
+++ src/components/button/textBtn/TextBtn.tsx
@@ -3,13 +3,13 @@ import { BtnStyleMap, BtnSizeMap } from './TextBtnStyle';
 interface TextBtnProps {
 	btnText: string;
 	onClick: () => void;
-	type: 'red' | 'black' | 'gray';
+	color: 'red' | 'black' | 'gray';
 	size: 'medium' | 'large';
 }
 
-const TextBtn = ({ btnText, onClick, type, size }: TextBtnProps) => {
+const TextBtn = ({ btnText, onClick, color, size }: TextBtnProps) => {
 	return (
-		<button css={[BtnSizeMap[size], BtnStyleMap[type]]} onClick={onClick} type="button">
+		<button css={[BtnSizeMap[size], BtnStyleMap[color]]} onClick={onClick} type="button">
 			{btnText}
 		</button>
 	);
