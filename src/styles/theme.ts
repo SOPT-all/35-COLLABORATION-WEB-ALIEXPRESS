a2571e5db200a254e70886a37cacfe5f038c22f0
bykbyk0401 <bykbyk0401@gmail.com>
2024-11-19 00:42:27 +0900

    style: color theme 설정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,54 @@
 import { css } from "@emotion/react";
 
+const COLOR = {
+    // Solid Colors
+    black: "#000000",
+    white: "#FFFFFF",
+    black30: "rgba(0, 0, 0, 0.3)",
+    black60: "rgba(0, 0, 0, 0.6)",
+
+    successPrimary: "#009966",
+    successSecondary: "#D7E8BF",
+    successBg: "#EAEFA6",
+
+    notificationPrimary: "#3170EE",
+    notificationSecondary: "#CDDCFB",
+    notificationBg: "#ECF1FE",
+
+    warningPrimary: "#FF5500",
+    warningSecondary: "#FADEC9",
+    warningBg: "#FFF2EB",
+
+    reviewStar: "#FFC816",
+
+    gray1: "#FAFAFA",
+    gray2: "#F5F5F5",
+    gray3: "#EBEBEB",
+    gray4: "#CCCCCC",
+    gray5: "#AFB2B4",
+    gray6: "#7C7C7F",
+    gray7: "#757575",
+    gray8: "#353639",
+    gray9: "#222222",
+    gray10: "#191919",
+
+    brandPrimary: "#FD384F",
+    brandDisable: "#FFB5BB",
+    brandBg: "#FEE7E7",
+
+    function1: "#E67D39",
+    function2: "#FAD4C0",
+    function3: "#FFF2EB",
+
+    magenta: "#FF0054",
+    red: "#D3021C",
+    orange: "#ED5628",
+    blue: "#4A8AB7",
+
+    // Gradient Colors
+    gradientOrange: "linear-gradient(90deg, #EE6930 0%, #EC4726 100%)",
+};
+
 const FONT = {
     kor: {
         // Title
@@ -251,5 +300,6 @@ const FONT = {
 };
 
 export const Theme = {
+    COLOR,
     FONT,
 };
