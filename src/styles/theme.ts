9098803b18374f253ef496ed24d16af452ba3c75
bykbyk0401 <bykbyk0401@gmail.com>
2024-11-18 18:29:45 +0900

    chore: 한글 font-weight 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,138 +4,138 @@ const FONT = {
     kor: {
         // Title
         titleBold20: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 20px;
             font-weight: 700;
+            font-size: 2rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 20px;
         `,
         // Body
         bodyBold16: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 16px;
             font-weight: 700;
+            font-size: 1.6rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 20px;
         `,
         bodyMedium16: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 16px;
             font-weight: 500;
+            font-size: 1.6rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 20px;
         `,
         bodyBold14: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 14px;
             font-weight: 700;
+            font-size: 1.4rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 17px;
         `,
         bodyMedium14: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 14px;
             font-weight: 500;
+            font-size: 1.4rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 17px;
         `,
         bodyBold13: css`
+            font-weight: 700;
+            font-size: 1.3rem;
             font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 13px;
-            font-weight: 600;
             line-height: 16px;
         `,
         bodySemibold13: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 13px;
             font-weight: 600;
+            font-size: 1.3rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 16px;
         `,
         // Caption
         captionBold12: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 12px;
             font-weight: 700;
+            font-size: 1.2rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 14px;
             text-transform: lowercase;
         `,
         captionSemibold12: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 12px;
             font-weight: 600;
+            font-size: 1.2rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 14px;
             text-transform: lowercase;
         `,
         captionMedium12: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 12px;
             font-weight: 500;
+            font-size: 1.2rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 14px;
             text-transform: lowercase;
         `,
         captionRegular12: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 12px;
             font-weight: 400;
+            font-size: 1.2rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 14px;
             text-transform: lowercase;
         `,
         captionReviewIndent12: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 12px;
             font-weight: 500;
+            font-size: 1.2rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 21px;
-            text-transform: lowercase;
             letter-spacing: 0;
+            text-transform: lowercase;
             text-indent: 80px;
         `,
         captionReview12: css`
+            font-weight: 500;
+            font-size: 1.2rem;
             font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 12px;
-            font-weight: 400;
             line-height: 21px;
             text-transform: lowercase;
         `,
         captionBold11: css`
+            font-weight: 700;
+            font-size: 1.1rem;
             font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 11px;
-            font-weight: 500;
             line-height: 13px;
             text-transform: lowercase;
         `,
         captionSemibold11: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 11px;
             font-weight: 600;
+            font-size: 1.1rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 13px;
             text-transform: lowercase;
         `,
         captionMedium11: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 11px;
             font-weight: 500;
+            font-size: 1.1rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 13px;
             text-transform: lowercase;
         `,
         captionBold10: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 10px;
             font-weight: 700;
+            font-size: 1rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 12px;
         `,
         captionMedium10: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 10px;
             font-weight: 500;
+            font-size: 1rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 12px;
             text-transform: lowercase;
         `,
         captionBold8: css`
-            font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 8px;
             font-weight: 700;
+            font-size: 0.8rem;
+            font-family: "Apple SD Gothic Neo", sans-serif;
             line-height: 8px;
             text-transform: lowercase;
         `,
         captionMedium8: css`
+            font-weight: 500;
+            font-size: 0.8rem;
             font-family: "Apple SD Gothic Neo", sans-serif;
-            font-size: 8px;
-            font-weight: 700;
             line-height: 8px;
             text-transform: lowercase;
         `,
