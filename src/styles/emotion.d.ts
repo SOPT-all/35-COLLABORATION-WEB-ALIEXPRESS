0f87a2a5b191bebec91637bb9e434376cf27c0dd
yarimu <yhli0320@naver.com>
2024-11-20 03:47:40 +0900

    fix: 불필요 코드 삭제

--- src/styles/emotion.d.ts
+++ src/styles/emotion.d.ts
@@ -1,9 +1,8 @@
 import '@emotion/react';
-import { ColorType, FontType } from './theme';
 
 declare module '@emotion/react' {
 	export interface Theme {
-		colors: ColorType;
-		fonts: FontType;
+		colors;
+		fonts;
 	}
 }
