75df8cd1551003b59f57edb8f6d12631987c725b
yarimu <yhli0320@naver.com>
2024-11-21 01:26:52 +0900

    fix: 로고 위치 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,19 @@
-import { ImgLogotypeL } from '@assets/icons/index';
+import { ImgLogotypeL, IcHamburgermenuWhite14 } from '@assets/icons/index';
 
-import { defaultStyle, headerStyle, logoOverlapStyle, logoDescStyle, fontEnStyle, fontKoStyle } from './HeaderStyle';
+import { headerStyle, logoDescStyle, fontEnStyle, fontKoStyle,  } from './HeaderStyle';
 
 const Header = () => (
-	<div css={[defaultStyle, headerStyle]}>
+	<div css={[headerStyle]}>
 		<div>
-			<ImgLogotypeL css={logoOverlapStyle} />
-			<div css={[defaultStyle, logoDescStyle]}>
+			<ImgLogotypeL />
+			<div css={logoDescStyle}>
 				<p css={fontEnStyle}>Alibaba Group</p>
 				<p css={fontKoStyle}>해외 직구 플랫폼</p>
 			</div>
 		</div>
-		<div>햄버거바 구역</div>
+		<div>
+			<IcHamburgermenuWhite14 />
+		</div>
 		<div>검색창구역</div>
 		<div>오른쪽 박스구역</div>
 	</div>
--- src/components/header/HeaderStyle.ts
+++ src/components/header/HeaderStyle.ts
@@ -1,13 +1,10 @@
 import { Theme, css } from '@emotion/react';
 
-export const defaultStyle = css`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-`;
-
 export const headerStyle = (theme: Theme) => css`
+	position: relative;
+	display: flex;
 	gap: 1.6rem;
+	justify-content: space-between;
 	width: 100%;
 	height: 5.4rem;
 	padding: 0.5rem 5.6rem 0.5rem 2.6rem;
@@ -17,15 +14,13 @@ export const headerStyle = (theme: Theme) => css`
 	background-color: ${theme.colors.gray10};
 `;
 
-export const logoOverlapStyle = css`
-	position: relative;
-`;
-
 export const logoDescStyle = css`
 	position: absolute;
-	top: 40px;
-	z-index: 50;
+	bottom: 0.7rem;
+	display: flex;
 	gap: 0.2rem;
+	align-items: center;
+	justify-content: center;
 `;
 
 export const fontEnStyle = (theme: Theme) => css`
