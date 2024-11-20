1927474bc7b55935da0e42ad7990fb5b0c903f0f
yarimu <yhli0320@naver.com>
2024-11-21 04:01:34 +0900

    refactor: 시맨틱태그 활용

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -34,62 +34,73 @@ import {
 } from './HeaderStyle';
 
 const Header = () => (
-	<div css={[relativeStyle, headerStyle]}>
-		<div>
+	<header css={[relativeStyle, headerStyle]}>
+		{/* 로고섹션 */}
+		<div role="banner">
 			<ImgLogotypeL />
 			<div css={logoDescStyle}>
 				<p css={fontEnStyle}>Alibaba Group</p>
 				<p css={fontKoStyle}>해외 직구 플랫폼</p>
 			</div>
 		</div>
-		<div css={hambergerStyle}>
-			<IcHamburgermenuWhite14 />
-		</div>
-		<div css={relativeStyle}>
-			<input type="text" css={inputStyle} placeholder="충전기" />
 
+		{/* 햄버거 메뉴 */}
+		<nav aria-label="Primary Navigation" css={hambergerStyle}>
+			<IcHamburgermenuWhite14 />
+		</nav>
+
+		{/* 검색섹션 */}
+		<div role="search" css={relativeStyle}>
+			<input type="text" css={inputStyle} placeholder="충전기" />
 			<div css={IconContainer}>
 				<IcImagesearchBlack24 />
 				<IcSearchWhite24 css={searchStyle} />
 			</div>
 		</div>
-		<div>
-			<div css={appLogLayout}>
-				<IcQrcodeWhite24 />
+
+		{/* 유틸리티 섹션 */}
+		{/* 앱 다운로드 */}
+		<section css={appLogLayout} aria-labelledby="app-download">
+			<IcQrcodeWhite24 />
+			<div css={textContainer}>
+				<p css={fontEnAppStyle}>AliExpress</p>
+				<p css={fontMStyle}>앱 다운로드</p>
+			</div>
+		</section>
+
+		{/* 국가 및 통화 */}
+		<section css={flagLayout} aria-labelledby="language-currency">
+			<ImgFlagKorL />
+			<div css={flagContainer}>
 				<div css={textContainer}>
-					<p css={fontEnAppStyle}>AliExpress</p>
-					<p css={fontMStyle}>앱 다운로드</p>
+					<p css={fontEnAppStyle}>KO/</p>
+					<p css={fontKRWStyle}>KRW</p>
 				</div>
+				<IcArrowbottomSWhite12 css={arrowStyle} />
 			</div>
-			<div css={flagLayout}>
-				<ImgFlagKorL />
-				<div css={flagContainer}>
-					<div css={textContainer}>
-						<p css={fontEnAppStyle}>KO/</p>
-						<p css={fontKRWStyle}>KRW</p>
-					</div>
-					<IcArrowbottomSWhite12 css={arrowStyle} />
-				</div>
-			</div>
-			<div css={appLogLayout}>
-				<IcUserWhite24 />
-				<div css={flagContainer}>
-					<div css={textContainer}>
-						<p css={fontMStyle}>환영합니다!/</p>
-						<p css={fontBStyle}>로그인/회원가입</p>
-					</div>
-					<IcArrowbottomSWhite12 css={arrowStyle} />
-				</div>
-			</div>
-			<div css={cartLayout}>
-				<IcCartWhite24 />
+		</section>
+
+		{/* 사용자 계정 */}
+		<section css={appLogLayout} aria-labelledby="user-account">
+			<IcUserWhite24 />
+			<div css={flagContainer}>
 				<div css={textContainer}>
-					<p css={[fontKRWStyle, numBackStyle]}>0</p>
-					<p css={fontMStyle}>장바구니</p>
+					<p css={fontMStyle}>환영합니다!/</p>
+					<p css={fontBStyle}>로그인/회원가입</p>
 				</div>
+				<IcArrowbottomSWhite12 css={arrowStyle} />
 			</div>
-		</div>
-	</div>
+		</section>
+
+		{/* 장바구니 */}
+		<section css={cartLayout} aria-labelledby="cart">
+			<IcCartWhite24 />
+			<div css={textContainer}>
+				<p css={[fontKRWStyle, numBackStyle]}>0</p>
+				<p css={fontMStyle}>장바구니</p>
+			</div>
+		</section>
+	</header>
 );
 
 export default Header;
