7b434a480342c07873f363722693bdf97dcc0fc7
Taew00k <davis0625@dgu.ac.kr>
2024-11-20 18:55:30 +0900

    feat: click시 icon 색깔 변경 기능 추가

--- src/components/button/recommendBtn/reviewBtnStyle.ts
+++ src/components/button/recommendBtn/reviewBtnStyle.ts
@@ -1,9 +1,30 @@
-import { css } from "@emotion/react";
+import { Theme, css } from "@emotion/react";
 
-export const ClickedBtnContainerStyle = css`
+export const FlexStyle = (theme: Theme) => css`
+display: flex;
+align-items: center;
+justify-content: center;
+white-space: nowrap;
+gap: 0.3rem;
+width: 9.2rem;
+height: 2.4rem;
+${theme.fonts.captionMedium12};
 `
 
-export const NonClickedBtnContainerStyle = css`
+export const ClickedBtnContainerStyle = (theme: Theme) => css`
+display: flex;
+box-sizing: border-box;
+padding : 0.4rem 0.8rem;
+border-radius: 0.4rem;
+color: ${theme.colors.notificationPrimary};
+`
+
+export const NonClickedBtnContainerStyle = (theme: Theme) => css`
+display: flex;
+box-sizing: border-box;
+padding : 0.4rem 0.8rem;
+border-radius: 0.4rem;
+
 `
 
 export const BtnStyleMap = {
