acfcba4a049cc35331fc47d27ab73c6022bb74a7
moonseonghui <msh9980@naver.com>
2024-11-21 07:18:57 +0900

    feat: 상품 상세정보 상단 ui 구조 구현

--- src/components/ProductInfo/ProductInfo.tsx
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -1,30 +1,118 @@
+import { IcInfoGray14, IcWarningBrandYellow16 } from '@assets/icons';
+import productBlack from '@assets/images/img_adapter_black.png';
+import productBlue from '@assets/images/img_adapter_blue.png';
+import productBlack2 from '@assets/images/img_adapter_cable_black.png';
+import productWhite2 from '@assets/images/img_adapter_cable_white.png';
+import productPink from '@assets/images/img_adapter_pink.png';
+import productPurple from '@assets/images/img_adapter_purple.png';
+import productWhite from '@assets/images/img_adapter_white.png';
+import bannerRewordImg from '@assets/images/img_banner_reward.png';
 import productSubImg1 from '@assets/images/img_product_sub1.png';
 import productSubImg2 from '@assets/images/img_product_sub2.png';
 import productSubImg3 from '@assets/images/img_product_sub3.png';
 import productSubImg4 from '@assets/images/img_product_sub4.png';
 import productSubImg5 from '@assets/images/img_product_sub5.png';
+import productSubImg6 from '@assets/images/img_product_sub6.png';
 import {
 	productInfoContainerStyle,
 	proudctImgLayoutStyle,
 	detailImgBoxStyle,
+	productImgStyle,
+	productDetailLayoutStyle,
+	productDetaiWrapperStyle,
+	priceBoxStyle,
+	priceDescriptionStyle,
+	productNameStyle,
+	mediumDividerStyle,
+	optionStyle,
+	extraInfoStyle,
+	optionImgStyle,
+	warnLayoutStyle,
+	warnTitleStyle,
+	warnDescriptionStyle,
 } from '@components/ProductInfo/ProductInfoStyle';
 import ProductInfoType from 'types/ProductInfoType';
 
-const productSubImages = [productSubImg1, productSubImg2, productSubImg3, productSubImg4, productSubImg5];
+const productSubImages = [
+	productSubImg1,
+	productSubImg2,
+	productSubImg3,
+	productSubImg4,
+	productSubImg5,
+	productSubImg6,
+];
+
+const productOptionImages = [
+	productBlack,
+	productBlack,
+	productBlack,
+	productWhite,
+	productWhite,
+	productPink,
+	productBlue,
+	productPurple,
+	productBlack2,
+	productWhite2,
+];
 
 interface ProductInfoProps extends ProductInfoType {}
 
-const ProductInfo = (props: ProductInfoProps) => (
-	<article css={productInfoContainerStyle}>
-		<section css={proudctImgLayoutStyle}>
-			<div css={detailImgBoxStyle}>
-				{productSubImages.map((imgSrc) => (
-					<img key={imgSrc} src={imgSrc} alt="product sub img" />
-				))}
+const ProductInfo = ({
+	productId,
+	productImg,
+	detail,
+	categoryName,
+	priceOriginal,
+	percent,
+	priceDiscount,
+	isCoupon,
+	reviewCount,
+	rating,
+}: ProductInfoProps) => (
+	<>
+		<article css={productInfoContainerStyle}>
+			<section css={proudctImgLayoutStyle}>
+				<div css={detailImgBoxStyle}>
+					{productSubImages.map((imgSrc) => (
+						<img key={imgSrc} src={imgSrc} alt="상품 상세 이미지" />
+					))}
+				</div>
+				<img css={productImgStyle} src={productSubImg1} alt="상품 대표 이미지" />
+			</section>
+			<section css={productDetailLayoutStyle}>
+				<img src={bannerRewordImg} alt="상품 리워드 배너" />
+				<div css={productDetaiWrapperStyle}>
+					<div>
+						<div css={priceBoxStyle}>7.52</div>
+						<div css={priceDescriptionStyle}>VAT가 포함된 가격 | 1% 추가 할인</div>
+					</div>
+					<div css={productNameStyle}>
+						Toocki GaN USB C 충전기, 맥쿡 아이폰 15, 14, 13, 12 프로 맥스, 삼성, 샤오미 포코용, 멀티 컬러, 고속 충전,
+						67W
+					</div>
+				</div>
+				<div css={mediumDividerStyle} />
+				<div css={optionStyle}>색상: 검정</div>
+				<section css={optionImgStyle}>
+					{productOptionImages.map((imgSrc, index) => (
+						<img key={`${imgSrc + index}`} src={imgSrc} alt="상품 옵션" />
+					))}
+				</section>
+				<div css={mediumDividerStyle} />
+				<div css={extraInfoStyle}>
+					<p>가격 정보 더보기</p>
+					<IcInfoGray14 />
+				</div>
+			</section>
+		</article>
+		<section css={warnLayoutStyle}>
+			<h1 css={warnTitleStyle}>경고 / 면책 조항</h1>
+			<div css={warnDescriptionStyle}>
+				<IcWarningBrandYellow16 />
+				충전 및 보관 시 잠재적인 화재 위험을 유의하시고, 반드시 사용 설명서를 엄격히 준수해 주세요.
 			</div>
-			<img src="" alt="" />
 		</section>
-	</article>
+	</>
 );
 
 export default ProductInfo;
--- src/components/ProductInfo/ProductInfoStyle.ts
+++ src/components/ProductInfo/ProductInfoStyle.ts
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import { css, Theme } from '@emotion/react';
 
 export const productInfoContainerStyle = css`
 	display: flex;
@@ -19,4 +19,98 @@ export const detailImgBoxStyle = css`
 	gap: 1.4rem;
 	width: 5rem;
 	height: 37rem;
+`;
+
+export const productImgStyle = css`
+	width: 36.8rem;
+	height: 36.8rem;
+`;
+
+export const productDetailLayoutStyle = css`
+	display: flex;
+	flex-direction: column;
+	gap: 1.6rem;
+	width: 50.9rem;
+	height: 39.7rem;
+`;
+
+export const productDetaiWrapperStyle = css`
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+	width: 100%;
+	height: 11rem;
+`;
+
+export const priceBoxStyle = css`
+	width: 100%;
+	height: 5.2rem;
+`;
+
+export const priceDescriptionStyle = (theme: Theme) => css`
+	${theme.fonts.captionMedium11}
+	color: ${theme.colors.gray6};
+`;
+
+export const productNameStyle = (theme: Theme) => css`
+	width: 100%;
+	height: 3.2rem;
+
+	${theme.fonts.captionBold12}
+`;
+
+export const mediumDividerStyle = (theme: Theme) => css`
+	width: 50.9rem;
+	height: 0.1rem;
+
+	background-color: ${theme.colors.gray3};
+`;
+
+export const optionStyle = (theme: Theme) => css`
+	${theme.fonts.kor.captionBold12}
+`;
+
+export const extraInfoStyle = (theme: Theme) => css`
+	display: flex;
+	gap: 0.5rem;
+	align-items: center;
+	width: 9.5rem;
+	height: 1.4rem;
+
+	color: ${theme.colors.gray6};
+	${theme.fonts.eng.captionMedium11}
+`;
+
+export const optionImgStyle = css`
+	display: grid;
+	grid-gap: 7px;
+	grid-template-columns: repeat(9, 1fr);
+	width: 100%;
+	height: 10.7rem;
+`;
+
+export const warnLayoutStyle = (theme: Theme) => css`
+	display: flex;
+	flex-direction: column;
+	gap: 0.8rem;
+	box-sizing: border-box;
+	width: 96rem;
+	height: 12.2rem;
+	padding: 2.4rem 0;
+
+	border-top: 1px solid ${theme.colors.gray3};
+	border-bottom: 1px solid ${theme.colors.gray3};
+`;
+
+export const warnTitleStyle = (theme: Theme) => css`
+	${theme.fonts.kor.titleBold20}
+	height: 4.6rem;
+`;
+
+export const warnDescriptionStyle = (theme: Theme) => css`
+	display: flex;
+	gap: 0.6rem;
+
+	${theme.fonts.kor.captionMedium11}
+	color: ${theme.colors.gray6};
 `;
