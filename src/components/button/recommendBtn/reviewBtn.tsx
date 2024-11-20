f385b1b867cee151d5890a7a0db1686ace35829c
Taew00k <davis0625@dgu.ac.kr>
2024-11-20 17:03:38 +0900

    feat: 리뷰 버튼 로직 구현

--- src/components/button/recommendBtn/reviewBtn.tsx
+++ src/components/button/recommendBtn/reviewBtn.tsx
@@ -1,12 +1,40 @@
+import { BtnClickedMap } from "./ReivewBtnStyle";
+import {useState} from "react";
+
+import { IcGoodGray16, IcHandshakeGray16, IcCometGray16  } from '@assets/icons';
+
 interface ReviewBtnProps{
-    btnText: string;
-    onClick: () => void;
-    isClicked: boolean;
+    type: "useful" | "recommend" | "like";
+    clickedCount: number;
+    
 }
 
-const ReviewBtn = ({btnText, onClick, isClicked}: ReviewBtnProps) =>{
+const ReviewBtn = ({type, clickedCount}: ReviewBtnProps) => {
+    const [isClicked, setIsClicked] = useState(false);
+    const [count, setCount] = useState(clickedCount);
 
+    const handleClick = (): void => {
+        setCount(isClicked ? count - 1 : count + 1); //클릭시 이미 클릭된 버튼이면 -1, 클릭이 안 됐던 버튼은 +1
+        setIsClicked(!isClicked); // 버튼 클릭 상태 바꾸기
+    };
 
+    const buttonType = {
+        useful: {icon: <IcGoodGray16 />, btnText:"유용해요" },
+        recommend: {icon: <IcCometGray16 />, btnText:"추천해요" },
+        like: {icon: <IcHandshakeGray16 />, btnText:"공감해요" }
+    };
+
+    const { icon, btnText } = buttonType[type];
+    
+    
+    return(
+        <button css = {BtnClickedMap[isClicked]}, onClick={handleClick} >
+            <span>{icon}</span>
+            <span>{btnText}</span>
+            <span>{count}</span>
+        </button>
+
+    );
 };
 
 export default ReviewBtn;
