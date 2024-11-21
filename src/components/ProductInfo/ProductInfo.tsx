b3c171c6dda5807a1995b96c7154433bc60c01ee
moonseonghui <msh9980@naver.com>
2024-11-22 03:50:07 +0900

    refactor: 상품관련 png파일 productDetailImages로 분리

--- src/components/ProductInfo/ProductInfo.tsx
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -1,18 +1,8 @@
 import { IcInfoGray14, IcWarningBrandYellow16 } from '@assets/icons';
-import productBlack from '@assets/images/img_adapter_black.png';
-import productBlue from '@assets/images/img_adapter_blue.png';
-import productBlack2 from '@assets/images/img_adapter_cable_black.png';
-import productWhite2 from '@assets/images/img_adapter_cable_white.png';
-import productPink from '@assets/images/img_adapter_pink.png';
-import productPurple from '@assets/images/img_adapter_purple.png';
-import productWhite from '@assets/images/img_adapter_white.png';
+
 import bannerRewordImg from '@assets/images/img_banner_reward.png';
 import productSubImg1 from '@assets/images/img_product_sub1.png';
-import productSubImg2 from '@assets/images/img_product_sub2.png';
-import productSubImg3 from '@assets/images/img_product_sub3.png';
-import productSubImg4 from '@assets/images/img_product_sub4.png';
-import productSubImg5 from '@assets/images/img_product_sub5.png';
-import productSubImg6 from '@assets/images/img_product_sub6.png';
+
 import {
 	productInfoContainerStyle,
 	proudctImgLayoutStyle,
@@ -38,27 +28,7 @@ import {
 } from '@components/ProductInfo/ProductInfoStyle';
 import ProductInfoType from 'types/ProductInfoType';
 
-const productSubImages = [
-	productSubImg1,
-	productSubImg2,
-	productSubImg3,
-	productSubImg4,
-	productSubImg5,
-	productSubImg6,
-];
-
-const productOptionImages = [
-	productBlack,
-	productBlack,
-	productBlack,
-	productWhite,
-	productWhite,
-	productPink,
-	productBlue,
-	productPurple,
-	productBlack2,
-	productWhite2,
-];
+import { productOptionImages, productSubImages } from '@assets/images/productDetailImages';
 
 interface ProductInfoProps extends ProductInfoType {}
 
