ac206bb05b5e5e0f6ac1dac0e215638c7d13a20c
Taew00k <davis0625@dgu.ac.kr>
2024-11-21 11:01:40 +0900

    remove: develop브랜치 충돌 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import tsconfigPaths from "vite-tsconfig-paths"
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tsconfigPaths(),
-    svgr({
-      svgrOptions: {
-        memo: true,
-      },
-      include: "**/*.svg",
-    }),],
-})
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		svgr({
+			svgrOptions: {
+				memo: true,
+			},
+			include: '**/*.svg',
+		}),
+	],
+});
