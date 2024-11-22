5f92208327501bae2ac89f3a52542cc8e2d639e0
yarimu <yhli0320@naver.com>
2024-11-22 22:00:53 +0900

    fix: 단순 렌더링 목데이터 객체 불변성 방식 수정

--- src/constants/footerMsg.ts
+++ src/constants/footerMsg.ts
@@ -1,4 +1,4 @@
-const MESSAGE = {
+const MESSAGE = Object.freeze({
 	FOOTER:
 		'지적 재산권 보호 - 개인정보 처리방침 - 위치 지도 - 이용 약관 - EU/UK 소비자를 위한 거래 서비스 계약 EU/EEA/UK 소비자를 위한 약관 - 사용자 정보 법적 문의 가이드 © 2010-2024 AliExpress.com. All rights reserved.',
 	HELP: '고객센터, 분쟁 및 신고, 구매자 보호, 지적 재산권 침해 신고, 규제 정보, 윤리 준수, 투명성 센터, 비로그인 불만 접수 창구',
@@ -6,6 +6,6 @@ const MESSAGE = {
 	CATEGORY: '모든 인기 상품, 제품, 프로모션, 저렴한 가격, 높은 가치, 리뷰',
 	GROUP:
 		'Alibaba 그룹 웹사이트, AliExpress, Alimama, Alipay, Fliggy, Alibaba Cloud, Alibaba International, AliTelecom, DingTalk, Juhuasuan, Taobao 마켓플레이스, Tmall, Taobao Global, AliOS, 1688',
-} as const;
+});
 
 export default MESSAGE;
