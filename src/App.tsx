f3e0d161348752381cbf2f9ab4598e7b00933170
Taew00k <davis0625@dgu.ac.kr>
2024-11-23 01:29:40 +0900

    fix: ReviewBtn  화살표 함수로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import ReviewBtn from '@components/button/recommendBtn/reviewBtn';
 import { Global, ThemeProvider } from '@emotion/react';
-import Theme from './styles/theme';
-import GLOBALSTYLE from './styles/global';
 
-const App = () => {
+import GLOBALSTYLE from './styles/global';
+import Theme from './styles/theme';
+
+function App() {
 	return (
-		<>
-			<ThemeProvider theme={Theme}>
-				<Global styles={GLOBALSTYLE} />
-			</ThemeProvider>
-		</>
+		<ThemeProvider theme={Theme}>
+			<Global styles={GLOBALSTYLE} />
+			<ReviewBtn type="recommend" clickedCount={15} />
+		</ThemeProvider>
 	);
-};
+}
 
 export default App;
--- src/components/button/recommendBtn/reviewBtn.tsx
+++ src/components/button/recommendBtn/reviewBtn.tsx
@@ -1,6 +1,3 @@
-import { flexStyle, btnStyleMap, countStyle } from './reviewBtnStyle';
-import { useState } from 'react';
-
 import {
 	IcGoodGray16,
 	IcHandshakeGray16,
@@ -9,6 +6,9 @@ import {
 	IcHandshakeBlue16,
 	IcCometBlue16,
 } from '@assets/icons';
+import { useState } from 'react';
+
+import { flexStyle, btnStyleMap, countStyle } from './reviewBtnStyle';
 
 interface ReviewBtnProps {
 	type: 'useful' | 'recommend' | 'like';
@@ -18,14 +18,11 @@ interface ReviewBtnProps {
 const ReviewBtn = ({ type, clickedCount }: ReviewBtnProps) => {
 	const [isClicked, setIsClicked] = useState(false);
 	const [count, setCount] = useState(clickedCount);
-
 	const handleClick = (): void => {
-		setCount(isClicked ? count - 1 : count + 1); //클릭시 이미 클릭된 버튼이면 -1, 클릭이 안 됐던 버튼은 +1
+		setCount(isClicked ? count - 1 : count + 1); // 클릭시 이미 클릭된 버튼이면 -1, 클릭이 안 됐던 버튼은 +1
 		setIsClicked(!isClicked); // 버튼 클릭 상태 바꾸기
 	};
-
 	const formatCount = (value: number) => (value > 999 ? '999+' : value);
-
 	const buttonType = {
 		useful: {
 			icon: isClicked ? <IcGoodBlue16 /> : <IcGoodGray16 />,
@@ -40,7 +37,6 @@ const ReviewBtn = ({ type, clickedCount }: ReviewBtnProps) => {
 			btnText: '공감해요',
 		},
 	};
-
 	const { icon, btnText } = buttonType[type];
 
 	return (
