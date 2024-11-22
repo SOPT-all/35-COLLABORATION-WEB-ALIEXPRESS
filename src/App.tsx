9dc8e54f0f179c64748362f751034b3fd1fe25f6
Taewook <127061738+Taew00k@users.noreply.github.com>
2024-11-23 01:43:01 +0900

    fix : App.tsx 충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Global, ThemeProvider } from '@emotion/react';
+import Route from '@route/Route';
+import GLOBALSTYLE from '@styles/global';
+import Theme from '@styles/theme';
 
-import GLOBALSTYLE from './styles/global';
-import Theme from './styles/theme';
-
-const App = () => {
+const App = () => (
 	<ThemeProvider theme={Theme}>
 		<Global styles={GLOBALSTYLE} />
-	</ThemeProvider>;
-};
+		<Route />
+	</ThemeProvider>
+);
 
 export default App;
