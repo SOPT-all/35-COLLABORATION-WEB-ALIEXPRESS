70f3b4e300212c17b70361b1873ecba0558ef712
Taew00k <davis0625@dgu.ac.kr>
2024-11-23 01:40:11 +0900

    remove: 불필요한 파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import ReviewBtn from '@components/button/recommendBtn/reviewBtn';
 import { Global, ThemeProvider } from '@emotion/react';
 
 import GLOBALSTYLE from './styles/global';
 import Theme from './styles/theme';
 
-function App() {
-	return (
-		<ThemeProvider theme={Theme}>
-			<Global styles={GLOBALSTYLE} />
-			<ReviewBtn type="recommend" clickedCount={15} />
-		</ThemeProvider>
-	);
-}
+const App = () => {
+	<ThemeProvider theme={Theme}>
+		<Global styles={GLOBALSTYLE} />
+	</ThemeProvider>;
+};
 
 export default App;
