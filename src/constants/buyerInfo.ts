d3f403cd859411fa3906c297923a362d14411b3f
Taew00k <davis0625@dgu.ac.kr>
2024-11-22 22:24:15 +0900

    feat: 구매자 정보 컴포넌트 구현

---
+++ src/constants/buyerInfo.ts
@@ -0,0 +1,5 @@
+export const buyerInfo = {
+	name: '데2걸',
+	phone: '+82 01029348372',
+	address: 'Jangan-gu, Suwon-si, Gyeonggi-do, Korea',
+} as const;
---
+++ src/pages/orderDetail/orderInfo/buyerInfo/buyerInfoStyle.ts
@@ -0,0 +1,23 @@
+import { Theme, css } from '@emotion/react';
+
+export const buyerInfoComponentStyle = css`
+	display: flex;
+	gap: 1.7rem;
+	width: 44.8rem;
+	height: 10.8rem;
+	padding: 1.6rem 2.4rem;
+`;
+
+export const infoDetailStyle = css`
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+`;
+
+export const korStringStyle = (theme: Theme) => css`
+	${theme.fonts.kor.bodyMedium14}
+`;
+
+export const engStringStyle = (theme: Theme) => css`
+	${theme.fonts.eng.bodyBold14}
+`;
