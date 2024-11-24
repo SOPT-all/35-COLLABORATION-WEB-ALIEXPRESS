2f597886eb001860a5f0616eb374c1bb48e18e13
yarimu <yhli0320@naver.com>
2024-11-24 18:26:51 +0900

    fix: 시맨틱태그 활용

--- src/components/productPage/review/review/FilterBtn.tsx
+++ src/components/productPage/review/review/FilterBtn.tsx
@@ -11,7 +11,7 @@ const FilterBtn = () => {
 	const [isHovered, setIsHovered] = useState(false);
 
 	return (
-		<div css={fontStyle}>
+		<button type="button" css={fontStyle}>
 			<div css={FilterBtnContainer} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
 				<p>최근 작성된 리뷰</p>
 				<IcArrowbottomGray12 />
@@ -24,7 +24,7 @@ const FilterBtn = () => {
 					<p css={listStyle}>재구매 리뷰</p>
 				</div>
 			)}
-		</div>
+		</button>
 	);
 };
 
