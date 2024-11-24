21e7b6ea00ba5bbb6341b5a9d17b12cbf8c4d844
moonseonghui <msh9980@naver.com>
2024-11-24 22:35:00 +0900

    feat: 구매관련 상세 텍스트 퍼블리싱

--- src/components/productSummary/ProductSummaryStyle.ts
+++ src/components/productSummary/ProductSummaryStyle.ts
@@ -5,31 +5,66 @@ export const ProductSummayContainer = css`
 `;
 
 export const productInfoLayoutStyle = css`
-	width: 96rem;
-	height: 26rem;
 	display: flex;
 	flex-direction: column;
 	gap: 2.4rem;
 	align-items: center;
+	width: 96rem;
+	height: 26rem;
 	margin-bottom: 2.4rem;
 `;
 
 export const productImgLayoutStyle = (theme: Theme) => css`
-	width: 96rem;
-	height: 111.3rem;
-	border-top: 1px solid ${theme.colors.gray3};
-	border-bottom: 1px solid ${theme.colors.gray3};
-	padding: 2.4rem 0;
 	display: flex;
 	flex-direction: column;
-	align-items: center;
 	gap: 2.4rem;
+	align-items: center;
+	width: 96rem;
+	height: 111.3rem;
+	margin-bottom: 2.4rem;
+	padding: 2.4rem 0;
+
+	border-top: 1px solid ${theme.colors.gray3};
+	border-bottom: 1px solid ${theme.colors.gray3};
 `;
 
 export const titleStyle = (theme: Theme) => css`
+	display: flex;
+	align-items: center;
 	width: 100%;
 	height: 4.6rem;
-	align-content: center;
+	margin-bottom: 0.8rem;
 
 	${theme.fonts.kor.titleBold20}
+`;
+
+export const purchaseInquiryLayoutStyle = (theme: Theme) => css`
+	width: 96rem;
+	height: 13.4rem;
+	margin-bottom: 2.4rem;
+
+	border-bottom: 1px solid ${theme.colors.gray3};
+`;
+
+export const agencyServiceInfoLayoutStyle = (theme: Theme) => css`
+	width: 96rem;
+	height: 9.8rem;
+	margin-bottom: 2.4rem;
+
+	border-bottom: 1px solid ${theme.colors.gray3};
+`;
+
+export const textBoxStyle = css`
+	display: flex;
+	flex-direction: column;
+	gap: 1.6rem;
+`;
+
+export const textStyle = (theme: Theme) => css`
+	display: flex;
+	gap: 0.8rem;
+	align-items: center;
+	height: 2rem;
+
+	${theme.fonts.eng.captionBold11}
 `;
--- src/components/productSummary/ProductSummay.tsx
+++ src/components/productSummary/ProductSummay.tsx
@@ -1,26 +1,50 @@
-import { DividerMedium } from '@assets/icons';
-import detailIma from '@assets/images/img_detail_xl.png';
+import detailImg from '@assets/images/img_detail_xl.png';
 import TextBtn from '@components/button/textBtn/TextBtn';
 import InfoTable from '@components/infoTable/InfoTable';
 import {
+	agencyServiceInfoLayoutStyle,
 	productImgLayoutStyle,
 	productInfoLayoutStyle,
+	purchaseInquiryLayoutStyle,
 	titleStyle,
+	textStyle,
+	textBoxStyle,
 } from '@components/productSummary/ProductSummaryStyle';
+import { IcAskBlack20, IcHandshakeGray20 } from '@assets/icons';
 
 const ProductSummay = () => (
 	<article>
 		<section css={productInfoLayoutStyle}>
-			<DividerMedium />
 			<InfoTable />
 			<TextBtn btnText="더보기" color="gray" size="medium" />
 		</section>
 		<section css={productImgLayoutStyle}>
 			<div>
 				<h1 css={titleStyle}>개요</h1>
-				<img src={detailIma} alt="디테일 상품 이미지" />
+				<img src={detailImg} alt="디테일 상품 이미지" />
 			</div>
 			<TextBtn btnText="더보기" color="gray" size="medium" />
+		</section>
+		<section css={purchaseInquiryLayoutStyle}>
+			<h1 css={titleStyle}>구매 문의 (2)</h1>
+			<div css={textBoxStyle}>
+				<p css={textStyle}>
+					<IcAskBlack20 />
+					제품의 총 전력이 67W인지 궁금합니다.
+				</p>
+				<p css={textStyle}>
+					<IcAskBlack20 />
+					제품의 총 전력이 67W인지 궁금합니다.
+				</p>
+			</div>
+		</section>
+		<section css={agencyServiceInfoLayoutStyle}>
+			<h1 css={titleStyle}>구매대행 서비스 안내</h1>
+			<p css={textStyle}>
+				<IcHandshakeGray20 />
+				해당 제품은 구매대행을 통하여 유통되는 제품입니다.「전기용품 및 생활용품 안전관리법」에 따라 안전관리대상
+				제품으로 분류됩니다.
+			</p>
 		</section>
 	</article>
 );
