207c33617c3b962d87a3f76aae1b5ce783020c0c
bykbyk0401 <bykbyk0401@gmail.com>
2024-11-26 02:08:59 +0900

    fix: 상품 컴포넌트 가로크기 받기 & 호버 시 상황에 따른 크기조절

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -1,43 +1,78 @@
 import { IcArrowrightGray12 } from '@assets/icons';
 
 import FreeTag from './FreeTag';
-import { couponBtnStyle, imageContainer, imageStyle, priceContainer, productContainer, productDiscountStyle, productInfoContainer, productInfoWrapper, productNameStyle, productPriceStyle, productWonStyle, productWrapper, tagContainer } from './ProductCardStyle';
+import {
+	couponBtnStyle,
+	imageContainer,
+	imageStyle,
+	priceContainer,
+	productContainer,
+	productDiscountStyle,
+	productInfoContainer,
+	productInfoWrapper,
+	productNameStyle,
+	productPriceStyle,
+	productWonStyle,
+	productWrapper,
+	tagContainer,
+} from './ProductCardStyle';
 import StarBtn from '@components/button/starBtn/StarBtn';
 
 interface ProductProps {
-    image: string;
-    name: string;
-    price: number;
-    discountRate: number;
-    hasCoupon?: boolean;
-    rating: number;
-    reviewCount: number;
+	image: string;
+	name: string;
+	price: number;
+	discountRate: number;
+	hasCoupon?: boolean;
+	rating: number;
+	reviewCount: number;
+	width?: string;
+	hoverLarge?: boolean;
 }
 
-const ProductCard = ({ image, name, price, discountRate, hasCoupon = false, rating, reviewCount }: ProductProps) => (
-        <article css={productContainer}>
-            <div css={productWrapper}>
-                <div css={imageContainer}>
-                    <img css={imageStyle} src={image} alt={name} />
-                </div>
-                <div css={productInfoContainer}>
-                    <div css={productInfoWrapper}>
-                        <p css={productNameStyle}>{name}</p>
-                        <StarBtn rating={rating} reviewCount={reviewCount} isRatingVisible={false} />
-                        <div css={priceContainer}>
-                            <span css={productWonStyle}>₩</span>
-                            <span css={productPriceStyle}>{price.toLocaleString()}</span>
-                            <span css={productDiscountStyle}>{discountRate}%</span>
-                        </div>
-                    </div>               
-                    <ul css={tagContainer}>
-                        <li><FreeTag text='무료 배송' color='red'/></li>
-                        <li><FreeTag text='무료 반품' color='gray'/></li>
-                    </ul>
-                </div>
-                {hasCoupon && <button css={couponBtnStyle}>쿠폰 받기<IcArrowrightGray12/></button>}
-            </div>
-        </article>
+const ProductCard = ({
+	image,
+	name,
+	price,
+	discountRate,
+	hasCoupon = false,
+	rating,
+	reviewCount,
+	width = '20.2rem',
+	hoverLarge = true,
+}: ProductProps) => (
+	<article css={(theme) => productContainer(theme, width, hoverLarge)}>
+		<div css={productWrapper}>
+			<div css={imageContainer(width)}>
+				<img css={imageStyle} src={image} alt={name} />
+			</div>
+			<div css={productInfoContainer(hoverLarge)}>
+				<div css={productInfoWrapper}>
+					<p css={productNameStyle}>{name}</p>
+					<StarBtn rating={rating} reviewCount={reviewCount} isRatingVisible={false} />
+					<div css={priceContainer}>
+						<span css={productWonStyle}>₩</span>
+						<span css={productPriceStyle}>{price.toLocaleString()}</span>
+						<span css={productDiscountStyle}>{discountRate}%</span>
+					</div>
+				</div>
+				<ul css={tagContainer}>
+					<li>
+						<FreeTag text="무료 배송" color="red" />
+					</li>
+					<li>
+						<FreeTag text="무료 반품" color="gray" />
+					</li>
+				</ul>
+			</div>
+			{hasCoupon && (
+				<button css={couponBtnStyle}>
+					쿠폰 받기
+					<IcArrowrightGray12 />
+				</button>
+			)}
+		</div>
+	</article>
 );
 
 export default ProductCard;
