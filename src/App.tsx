f90b34287ea5324e7835f065bd96617e85e501e4
Taewook <127061738+Taew00k@users.noreply.github.com>
2024-11-26 19:18:31 +0900

    Revert "[REVERT] 주문 배송 페이지 주문 배송 정보 컴포넌트 퍼블리싱"

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { Global, ThemeProvider } from '@emotion/react';
 import Route from '@route/Route';
 import GLOBALSTYLE from '@styles/global';
 import Theme from '@styles/theme';
+import { OrderInfoComponent } from '@components/orderDetail/orderInfo/orderInfo';
 
 const App = () => (
 	<ThemeProvider theme={Theme}>
 		<Global styles={GLOBALSTYLE} />
-		<Route />
+		<OrderInfoComponent />
 	</ThemeProvider>
 );
 
