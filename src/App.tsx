622b121aea387e601c9ca115c5a27276827755ac
Taew00k <davis0625@dgu.ac.kr>
2024-11-26 18:11:53 +0900

    remove: app.tsx 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import { Global, ThemeProvider } from '@emotion/react';
 import Route from '@route/Route';
 import GLOBALSTYLE from '@styles/global';
 import Theme from '@styles/theme';
-import { SideBarComponent } from '@components/orderDetail/sideBar/sideBar';
 
 const App = () => (
 	<ThemeProvider theme={Theme}>
 		<Global styles={GLOBALSTYLE} />
-		<SideBarComponent />
+		<Route />
 	</ThemeProvider>
 );
 
