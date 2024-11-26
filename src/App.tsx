f4200f8bcc7acdd4930e2a208cf66a029df629e8
Taew00k <davis0625@dgu.ac.kr>
2024-11-26 16:07:31 +0900

    remove: App.tsx 테스트 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import { Global, ThemeProvider } from '@emotion/react';
 import Route from '@route/Route';
 import GLOBALSTYLE from '@styles/global';
 import Theme from '@styles/theme';
-import OrderHistory from '@components/orderDetail/orderHistory/orderHistory';
 
 const App = () => (
 	<ThemeProvider theme={Theme}>
 		<Global styles={GLOBALSTYLE} />
-		<OrderHistory />
+		<Route />
 	</ThemeProvider>
 );
 
