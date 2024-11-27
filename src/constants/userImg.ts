4c3fed35cff306bca497af96373e0640953dcd3d
moonseonghui <msh9980@naver.com>
2024-11-28 02:56:18 +0900

    fix: build 오류 수정

--- src/constants/userImg.ts
+++
@@ -1,5 +0,0 @@
-import { ImgUser1, ImgUser2, ImgUser3, ImgUser4, ImgUser5, ImgUser6, ImgUser7 } from '@assets/icons/index';
-
-const profileImages = [ImgUser1, ImgUser2, ImgUser3, ImgUser4, ImgUser5, ImgUser6, ImgUser7];
-
-export default profileImages;
