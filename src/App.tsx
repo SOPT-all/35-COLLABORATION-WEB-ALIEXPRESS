2227961a1e464f9a30aa753fbed8cabc86b64f76
Taew00k <davis0625@dgu.ac.kr>
2024-11-27 23:55:57 +0900

    remove: 테스트 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import { Global, ThemeProvider } from '@emotion/react';
 import Route from '@route/Route';
 import GLOBALSTYLE from '@styles/global';
 import Theme from '@styles/theme';
-import ProductRecommendComponent from '@components/orderDetail/productRecommend/productRecommend';
 
 const App = () => (
 	<ThemeProvider theme={Theme}>
 		<Global styles={GLOBALSTYLE} />
-		<ProductRecommendComponent />
+		<Route />
 	</ThemeProvider>
 );
 
