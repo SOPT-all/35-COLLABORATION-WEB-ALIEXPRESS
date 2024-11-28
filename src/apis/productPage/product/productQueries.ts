8b6015e707211a3e4d644fb34ded2297d0d2548c
bykbyk0401 <bykbyk0401@gmail.com>
2024-11-28 18:47:39 +0900

    chore: 안쓰는 값 삭제

--- src/apis/productPage/product/productQueries.ts
+++ src/apis/productPage/product/productQueries.ts
@@ -1,10 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { AxiosError } from 'axios';
 import fetchRelatedProducts from './getProduct';
-
-interface ApiError {
-	message: string;
-}
 
 const useRelatedProducts = (productId: number) => {
 	return useQuery({
